f291371d491432c60f25640c33713833cd4ff422
JonasBjorhei <jonas.kjeks@gmail.com>
2023-01-20 15:08:38 +0100

    Fix: Got it to compile, was buggy after merge

--- lost-in-translation/src/views/UserPage.jsx
+++ lost-in-translation/src/views/UserPage.jsx
@@ -1,20 +1,21 @@
-import withAuth from ".../hoc/withAuth"
+// import withAuth from ".../hoc/withAuth"
 import UserActions from "../components/User/UserAction";
-import UserHeader from "../components/User/UserHeader";
-import UserTranslationHistory from "../components/User/UserTranslationHistory";
+// import UserHeader from "../components/User/UserHeader";
+// import UserTranslationHistory from "../components/User/UserTranslationHistory";
 
-const User = () => {
+ const UserPage = () => {
 
-const { user } = useUser()
+// const { user } = useUser()
 
-    return (
+//     return (
         <>
             <h1>User</h1>
-            <UserHeader username={user.username}/>
+            {/* <UserHeader username={user.username}/> */}
             <UserActions />
-            <UserTranslationHistory translations={user.translations}/>
+            {/* <UserTranslationHistory translations={user.translations}/> */}
         </>
-    )
-}
+//     )
+ }
 
-export default withAuth(User);
+// export default withAuth(User);
+export default UserPage;
