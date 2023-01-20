7a2db7217a3f68200c103dbcbdce487b0ed6cff3
JonasBjorhei <jonas.kjeks@gmail.com>
2023-01-20 15:37:27 +0100

    fix: works fine
    Need some more of the base code to get it all set up

--- lost-in-translation/src/views/UserPage.jsx
+++ lost-in-translation/src/views/UserPage.jsx
@@ -1,19 +1,21 @@
 // import withAuth from ".../hoc/withAuth"
-import UserActions from "../components/User/UserAction";
+//simport UserActions from "../components/User/UserAction";
 // import UserHeader from "../components/User/UserHeader";
 // import UserTranslationHistory from "../components/User/UserTranslationHistory";
 
  const UserPage = () => {
-
+    return(
+    <h1>User</h1>
+    )
 // const { user } = useUser()
 
 //     return (
-        <>
-            <h1>User</h1>
-            {/* <UserHeader username={user.username}/> */}
-            <UserActions />
-            {/* <UserTranslationHistory translations={user.translations}/> */}
-        </>
+        // <>
+        //     
+        //     <UserHeader username={user.username}/>
+        //     <UserActions />
+        //     <UserTranslationHistory translations={user.translations}/>
+        // </>
 //     )
  }
 
