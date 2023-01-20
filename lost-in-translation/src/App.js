2ba3b4eebee3626cebf546d70e713efb532d4ef8
Larsjoar96 <32615350+Larsjoar96@users.noreply.github.com>
2023-01-20 14:48:21 +0100

    Made link paths
    Made paths to the different pages we need

--- lost-in-translation/src/App.js
+++ lost-in-translation/src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
-
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from 'react-router-dom'
+import LoginPage from './views/LoginPage';
+import TranslationPage from './views/TranslationPage';
+import UserPage from './views/UserPage';
 function App() {
 
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path = "/" element = {<LoginPage/>}/>
+          <Route path = "/Translation" element = {<TranslationPage/>}/>
+          <Route path = "/User" element = {<UserPage/>}/>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
